f8ba82f73137e5231563fea00a0bd72902b1f4d0
DariusG101 <107132490+DariusG101@users.noreply.github.com>
2022-06-21 10:05:39 -0500

    Initial commit

---
+++ marathon.py
@@ -0,0 +1,33 @@
+# """
+#  *****************************************************************************
+#    FILE:        marathon.py
+#
+#    AUTHOR:      {Your Name Here}
+#
+#    ASSIGNMENT: A marathon calculator to determine if a U.S. participant can
+#    run in the Tokyo Marathon. 
+#
+#    DATE:         {Today's Date}
+#
+#    DESCRIPTION: {Your Description Here}
+#
+#  *****************************************************************************
+
+# $1 = 134.28¥
+JAPANESE_YEN = 134.28
+
+# 1 mi = 1609.34 meters
+METERS_IN_MILE = 1609.34 
+
+# Insert Code Below
+def fitness():
+
+    # This ignores our code so that we do not get an error Remove 'pass' when you want to start testing code.
+    
+    pass
+    
+
+# This invokes the main function.  It is always included in our
+# python programs
+if __name__ == "__main__":
+    fitness()
